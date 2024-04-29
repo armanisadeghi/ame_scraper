9b1eb563e52abd4a9fc0456b467f1c1439197d36
Igor B <tellprices@gmail.com>
2024-04-29 17:22:27 +0200

    minor testing changes

--- ame_scraper/pipelines.py
+++ ame_scraper/pipelines.py
@@ -31,7 +31,7 @@ class AiDreamScraperPipeline:
             current_time = datetime.datetime.now()  # Assuming datetime has been imported
             execute_sql(self.conn, """
                 INSERT INTO webpages (url, domain_id, access_timestamp, http_status, last_updated) VALUES (?, ?, ?, ?, ?)
-                """, (item["url"], domain_id, current_time, 200, current_time))  # Assuming HTTP status 200 and the current timestamp
+                ON CONFLICT(url) DO UPDATE SET domain_id=excluded.domain_id, access_timestamp=excluded.access_timestamp, http_status=excluded.http_status, last_updated=excluded.last_updated""", (item["url"], domain_id, current_time, 200, current_time))  # Assuming HTTP status 200 and the current timestamp
             page_id = execute_sql(self.conn, "SELECT last_insert_rowid()", fetch_one=True)[0]
 
         content = item["clean_body"]
@@ -50,28 +50,31 @@ class AiDreamScraperPipeline:
             execute_sql(self.conn, "INSERT INTO links (page_id, text, href) VALUES (?, ?, ?)", (page_id, link['text'], link['href']))
 
         # Process images
-        image_data = [(page_id[0], img['src'], img['alt']) for img in item["images"]]
+        try:
+            image_data = [(page_id, img['src'], img['alt']) for img in item["images"]]
+        except:
+            print()
         for img in image_data:
             execute_sql(self.conn, "INSERT INTO images (page_id, src, alt) VALUES (?, ?, ?)", img)
 
         # Insert HeadingStructure data
         if "header_structure" in item:
             header_structure_json = json.dumps(item["header_structure"])
-            execute_sql(self.conn, "INSERT INTO heading_structures (result_id, data) VALUES (?, ?)", (page_id[0], header_structure_json))
+            execute_sql(self.conn, "INSERT INTO heading_structures (result_id, data) VALUES (?, ?) on conflict do update set data=excluded.data", (page_id, header_structure_json))
 
         # Insert StructuredData data
         if "schema_org_data" in item:
             schema_org_json = json.dumps(item["schema_org_data"])
-            execute_sql(self.conn, "INSERT INTO structured_data (result_id, schema_org_data) VALUES (?, ?)", (page_id[0], schema_org_json))
+            execute_sql(self.conn, "INSERT INTO structured_data (result_id, schema_org_data) VALUES (?, ?) on conflict (result_id) do update set schema_org_data=excluded.schema_org_data", (page_id, schema_org_json))
 
         # Insert SitemapContent data
         if "sitemap_contents" in item:
             for sitemap_content in item["sitemap_contents"]:
-                execute_sql(self.conn, "INSERT INTO sitemap_contents (domain_id, content) VALUES (?, ?)", (domain_id[0], sitemap_content))
+                execute_sql(self.conn, "INSERT INTO sitemap_contents (domain_id, content) VALUES (?, ?)", (domain_id, sitemap_content))
 
         # Insert SitemapURL data
         if "sitemap_links" in item:
             for sitemap_link in item["sitemap_links"]:
-                execute_sql(self.conn, "INSERT INTO sitemap_urls (domain_id, url) VALUES (?, ?)", (domain_id[0], sitemap_link))
+                execute_sql(self.conn, "INSERT INTO sitemap_urls (domain_id, url) VALUES (?, ?)", (domain_id, sitemap_link))
 
         return item
